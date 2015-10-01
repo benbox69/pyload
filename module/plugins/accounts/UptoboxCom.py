7fd3a8a6fe7a0ec453f4e8a1eb0a9944a373b67f
benbox69 <dev@tollet.me>
2015-10-01 17:54:27 +0200

    Update UptoboxCom.py

--- module/plugins/accounts/UptoboxCom.py
+++ module/plugins/accounts/UptoboxCom.py
@@ -17,3 +17,33 @@ class UptoboxCom(XFSAccount):
 	HOSTER_DOMAIN = "uptobox.com"
 	HOSTER_URL    = "https://uptobox.com/"
 	LOGIN_URL     = "https://login.uptobox.com/"
+	
+	# -*- coding: utf-8 -*-
+
+import json
+
+from module.plugins.internal.XFSAccount import XFSAccount
+
+
+class UptoboxCom(XFSAccount):
+	__name__    = "UptoboxCom"
+	__type__    = "account"
+	__version__ = "0.14"
+	__status__  = "testing"
+
+	__description__ = "Uptobox.com account plugin"
+	__license__     = "GPLv3"
+	__authors__     = [("benbox69", "dev@tollet.me")]
+
+
+	HOSTER_DOMAIN = "uptobox.com"
+	HOSTER_URL    = "https://uptobox.com/"
+	LOGIN_URL     = "https://login.uptobox.com/logarithme"
+
+	def signin(self, user, password, data):
+
+		jsonstring = self.load(self.LOGIN_URL, None,  post={'login': user, 'password': password, 'op': 'login'})
+		parsedjson = json.loads(jsonstring)
+		
+		if parsedjson['success'] is None:
+			self.login_fail()
