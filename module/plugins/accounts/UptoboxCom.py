73417244abec25c665d4bad1dfeda05b709d8282
benbox69 <dev@tollet.me>
2015-10-01 17:55:00 +0200

    Update UptoboxCom.py

--- module/plugins/accounts/UptoboxCom.py
+++ module/plugins/accounts/UptoboxCom.py
@@ -1,25 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from module.plugins.internal.XFSAccount import XFSAccount
-
-
-class UptoboxCom(XFSAccount):
-	__name__    = "UptoboxCom"
-	__type__    = "account"
-	__version__ = "0.14"
-	__status__  = "testing"
-
-	__description__ = "Uptobox.com account plugin"
-	__license__     = "GPLv3"
-	__authors__     = [("benbox69", "dev@tollet.me")]
-
-
-	HOSTER_DOMAIN = "uptobox.com"
-	HOSTER_URL    = "https://uptobox.com/"
-	LOGIN_URL     = "https://login.uptobox.com/"
-	
-	# -*- coding: utf-8 -*-
-
 import json
 
 from module.plugins.internal.XFSAccount import XFSAccount
