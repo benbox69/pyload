ea1a98eac447d38046ca67c8da8cc34615a36a63
benbox69 <dev@tollet.me>
2015-10-01 18:00:44 +0200

    Update UptoboxCom.py

--- module/plugins/accounts/UptoboxCom.py
+++ module/plugins/accounts/UptoboxCom.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import json
+import re
+import time
 
-from module.plugins.internal.XFSAccount import XFSAccount
+from module.plugins.internal.Account import Account
 
 
-class UptoboxCom(XFSAccount):
+class UptoboxCom(Account):
 	__name__    = "UptoboxCom"
 	__type__    = "account"
-	__version__ = "0.14"
+	__version__ = "0.10"
 	__status__  = "testing"
 
-	__description__ = "Uptobox.com account plugin"
+	__description__ = """Uptobox.com account plugin"""
 	__license__     = "GPLv3"
 	__authors__     = [("benbox69", "dev@tollet.me")]
 
@@ -19,6 +21,46 @@ class UptoboxCom(XFSAccount):
 	HOSTER_DOMAIN = "uptobox.com"
 	HOSTER_URL    = "https://uptobox.com/"
 	LOGIN_URL     = "https://login.uptobox.com/logarithme"
+
+	VALID_UNTIL_PATTERN = r'Premium-Account expire: (\d{1,2} [\w^_]+ \d{4})'
+
+	def grab_info(self, user, password, data):
+
+		validuntil   = None
+        	trafficleft  = None
+       		premium      = None
+
+		data = self.get_data(user)
+		html = self.load(self.HOSTER_URL, get={'op': "my_account"})
+
+		p = re.compile(self.VALID_UNTIL_PATTERN)
+		
+		m = re.search(p, html)
+
+		if m:
+			expiredate = m.group(1).strip()
+			self.log_debug("Expire date: " + expiredate)
+
+            		try:
+                		validuntil = time.mktime(time.strptime(expiredate, "%d %B %Y"))
+
+            		except Exception, e:
+                		self.log_error(e)
+
+            		else:
+                		self.log_debug("Valid until: %s" % validuntil)
+
+                		if validuntil > time.mktime(time.gmtime()):
+                    			premium     = True
+                    			trafficleft = -1
+                		else:
+                    			premium    = False
+                    			validuntil = None  #: Registered account type (not premium)
+        	else:
+            		self.log_debug("VALID_UNTIL_PATTERN not found")
+
+		return {'validuntil': validuntil, 'trafficleft': trafficleft, 'premium': premium}
+
 
 	def signin(self, user, password, data):
 
